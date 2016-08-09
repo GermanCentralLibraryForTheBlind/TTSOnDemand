1deba2151ccee6888a9b5e77c6c40fc15d6548a1
Lars Voigt <lars.voigt@dzb.de>
2016-08-09 17:13:29 +0200

    - init

---
+++ js/dzb-tts-on-demand.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alan on 09.08.16.
+ */
+const newsUrl = "http://www.mdr.de/nachrichten/politik/ausland/putin-erdogan-treffen-petersburg-100.html";
+
+$( "#runTTS" ).click(generateTTS());
+
+
+function generateTTS() {
+    $( "#result" ).load( newsUrl + " box" );
+}
