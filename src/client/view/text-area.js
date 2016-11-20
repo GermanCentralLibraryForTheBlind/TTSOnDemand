0c6bb2afd3813ed919c8e48056f32e220e84f233
Lars Voigt <lars.voigt@gmx.de>
2016-11-20 22:57:56 +0100

    - added autoscroll function to scroll to highlighted content if it is not in viewport

--- src/client/view/text-area.js
+++ src/client/view/text-area.js
@@ -5,7 +5,7 @@ module.exports = {
     initialize: function () {
         var self = this;
         this.model.bind('change:current_text_element_id', function () {
-            self.highlight();
+            self.highlight(true);
         });
     },
 
@@ -13,7 +13,7 @@ module.exports = {
     // highlight the new ID and unhighlight the old one
     // annoyingly, using removeClass to unhighlight wasn't working in iframes
     // so rather than leave everything highlighted, we'll just make sure it's in view
-    highlight: function () {
+    highlight: function (scrollOn) {
         var id = this.model.get("current_text_element_id");
 
         if (this.model.get("should_highlight")) {
@@ -33,14 +33,38 @@ module.exports = {
             if (elm.length > 0) {
                 this.lastId = id;
 
-                //if ($("#" + id).is(":visible")) {
-                //    elm[0].scrollIntoView(true);
-                //}
+                if (scrollOn && !isOnScreen($("#" + id))) {
+
+                    const offset = $("#" + id).offset();
+                    $(document).scrollTop(offset.top - 60);
+                }
+
                 elm.addClass("highlight");
             }
         }
         function getTextElm(elmId) {
             return $("html").contents().find("#" + elmId);
+        }
+
+        function isOnScreen ($el) {
+
+            var win = $(window);
+
+            var viewport = {
+                top : win.scrollTop(),
+                left : win.scrollLeft()
+            };
+            viewport.right = viewport.left + win.width();
+            viewport.bottom = viewport.top + win.height();
+
+            var bounds = $el.offset();
+            bounds.right = bounds.left + $el.outerWidth();
+            bounds.bottom = bounds.top + $el.outerHeight();
+            // console.log(viewport);
+            // console.log(bounds);
+
+            return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
         }
     }
 };
